4a268944d19b7ca03f13b57d5a2b5bb8f2c75c54
yangjp22 <jinyang0604@gmail.com>
2022-06-11 18:31:35 -0400

    rdd filter api

---
+++ src/01_RDD/01_RDD_operator_filter.py
@@ -0,0 +1,15 @@
+# coding:utf8
+
+from pyspark import SparkContext, SparkConf
+import os
+
+os.environ["PYSPARK_PYTHON"] = "../../venv/bin/python"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "../../venv/bin/python"
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("test").setMaster("local[*]")
+    sc = SparkContext(conf=conf)
+
+    rdd = sc.parallelize([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    rdd_filtered = rdd.filter(lambda x: x % 2 == 0)
+    print(rdd_filtered.collect())
