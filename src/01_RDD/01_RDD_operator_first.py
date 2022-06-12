2192573bc512bc1600aa79cf78fc75d343f5089f
yangjp22 <jinyang0604@gmail.com>
2022-06-12 10:33:36 -0400

    rdd first api

---
+++ src/01_RDD/01_RDD_operator_first.py
@@ -0,0 +1,31 @@
+# coding:utf8
+
+from pyspark import SparkContext, SparkConf
+import os, functools
+
+os.environ["PYSPARK_PYTHON"] = "../../venv/bin/python"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "../../venv/bin/python"
+
+
+def twoGroups(x):
+    return "odd" if x % 2 != 0 else "even"
+
+
+def threeGroups(x):
+    if x % 3 == 0:
+        return "Exactly"
+    return "1_more" if x % 3 == 1 else "2_more"
+
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("test").setMaster("local[*]")
+    sc = SparkContext(conf=conf)
+
+    rdd = sc.parallelize([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)
+    print(rdd.first())
+
+    rdd_2 = sc.parallelize([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    print(rdd_2.first())
+
+    rdd_3 = sc.parallelize([('a', 2), ('b', 3)])
+    print(rdd_3.first())
