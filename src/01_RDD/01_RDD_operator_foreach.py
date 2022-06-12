4b3568d5ab2adb02c775affa21968d0f9ba911c8
yangjp22 <jinyang0604@gmail.com>
2022-06-12 12:02:59 -0400

    rdd foreach api

---
+++ src/01_RDD/01_RDD_operator_foreach.py
@@ -0,0 +1,18 @@
+# coding:utf8
+
+from pyspark import SparkContext, SparkConf
+import os
+
+os.environ["PYSPARK_PYTHON"] = "../../venv/bin/python"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "../../venv/bin/python"
+
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("test").setMaster("local[*]")
+    sc = SparkContext(conf=conf)
+
+    rdd = sc.parallelize(range(1, 10))
+
+    sum = 0
+    rdd.foreach(lambda x: print(x + sum))
+    print(rdd.collect())
