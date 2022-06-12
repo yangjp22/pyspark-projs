9964299a5706fea34c97a7595885e194c0f336f1
yangjp22 <jinyang0604@gmail.com>
2022-06-12 12:22:52 -0400

    rdd saveAsTextFile api

---
+++ src/01_RDD/01_RDD_operator_saveAsTextFile.py
@@ -0,0 +1,14 @@
+# coding:utf8
+
+from pyspark import SparkContext, SparkConf
+import os
+
+os.environ["PYSPARK_PYTHON"] = "../../venv/bin/python"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "../../venv/bin/python"
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("test").setMaster("local[*]")
+    sc = SparkContext(conf=conf)
+
+    rdd2 = sc.parallelize([('a', 1), ('a', 2), ('b', 1), ('b', 4), ('a', 3)])
+    rdd2.saveAsTextFile("../output.txt")
