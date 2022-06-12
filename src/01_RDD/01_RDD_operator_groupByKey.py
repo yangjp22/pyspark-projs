22f9b7061560574dda7bf3262855463271e2c7fe
yangjp22 <jinyang0604@gmail.com>
2022-06-11 21:02:42 -0400

    rdd groupByKey api

---
+++ src/01_RDD/01_RDD_operator_groupByKey.py
@@ -0,0 +1,20 @@
+# coding:utf8
+
+from pyspark import SparkContext, SparkConf
+import os
+
+os.environ["PYSPARK_PYTHON"] = "../../venv/bin/python"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "../../venv/bin/python"
+
+if __name__ == "__main__":
+    conf = SparkConf().setAppName("test").setMaster("local[*]")
+    sc = SparkContext(conf=conf)
+
+    rdd = sc.parallelize([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    print(rdd.collect())
+
+    rdd2 = sc.parallelize([('a', 1), ('a', 2), ('b', 1), ('b', 1), ('a', 2)])
+    print(rdd2.collect())
+
+    rdd3 = rdd2.groupByKey()
+    print(rdd3.map(lambda x: (x[0], list(x[1]))).collect())
